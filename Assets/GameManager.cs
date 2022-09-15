d49ea1f146468166a3ed7f4e91a839682e8a69e1
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-09-15 03:03:28 +0300

    GameManager created

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public int currentLevelIndex = 0;
+    [HideInInspector] public GameStatus currentGameStatus = GameStatus.Playing;
+
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public enum GameStatus
+    {
+        Playing,
+        Paused,
+        Failed,
+        Success
+    }
+}
