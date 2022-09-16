01cf228ad649d99e12db8bc45f520823d2d7d131
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-09-16 18:20:43 +0300

    Number cubes completed for level generation

--- Assets/Scripts/NumberCube.cs
+++ Assets/Scripts/NumberCube.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class NumberCube : MonoBehaviour
 {
     [SerializeField] private TextMeshPro numberText;
+    [SerializeField] private MeshRenderer meshRenderer;
 
     public NumberCubeData numberCubeData;
 
     void Start()
     {
         numberText.text = numberCubeData.cubeNumber.ToString();
+        meshRenderer.material.color = numberCubeData.cubeColor;
     }
 }
--- Assets/Scripts/NumberCubeData.cs
+++ Assets/Scripts/NumberCubeData.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 public class NumberCubeData : ScriptableObject
 {
     public int cubeNumber;
+    public Color cubeColor;
 }
