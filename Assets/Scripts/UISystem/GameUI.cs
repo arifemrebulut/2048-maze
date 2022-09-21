454f8ce487c50a3c7ad0f8f8c28a51b04e348958
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-09-22 00:57:03 +0300

    Level Number added to the game ui

--- Assets/Scripts/UISystem/GameUI.cs
+++ Assets/Scripts/UISystem/GameUI.cs
@@ -1,8 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour, IUIController
 {
     [field: SerializeField] public UIControllerType UIControllerType { get; set; }
+
+    [SerializeField] private TextMeshProUGUI levelText;
+
+    private void Start()
+    {
+        levelText.text = (GameManager.Instance.CurrentLevelIndex + 1).ToString();    
+    }
 
     public void Close()
     {
