05f29026939c7af41b1d9dc13aa25e6fdeb2ec2b
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-09-22 02:07:31 +0300

    Level creation system created

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,11 +10,22 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private ColorPrefabPairs colorPrefabPairs;
 
     [Space(10)]
-    [SerializeField] private GameObject levelParent;
-    [SerializeField] private GameObject levelBasePrefab;    
+    [SerializeField] private GameObject levelsParent;
+    [SerializeField] private GameObject levelBasePrefab;
+    [SerializeField] private Transform nextLevelInstantiatePoint;
+    [SerializeField] private Transform levelDestroyPoint;
 
+    [Header("Move Level Tween")]
+    [SerializeField] private float moveDuration;
+
+    private GameObject currentLevel;
+    private GameObject nextLevel;
+    private GameObject oldLevel;
+
+    private LevelData currentLevelData;
+    private LevelData nextLevelData;
     private int currentLevelIndex;
-    private LevelData currentLevel;
+    private int nextLevelIndex;
 
     public int initialPlayerNumber { get; private set; }
 
@@ -32,18 +44,42 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        currentLevelIndex = GameManager.Instance.CurrentLevelIndex;
-        currentLevel = levels[currentLevelIndex];
-        initialPlayerNumber = currentLevel.initialPlayerNumber;
-
-        SetupLevel();
+        EventManager.LevelSuccesEvent += MoveLevels;
     }
 
-    private void SetupLevel()
+    private void OnDisable()
     {
-        Texture2D levelTexture = currentLevel.levelTexture;
+        EventManager.LevelSuccesEvent -= MoveLevels;
+    }
+
+    private void Start()
+    {
+        SetupLevels();
+    }
+
+    private void SetupLevels()
+    {
+        GetLevelInformations();
+        CreateCurrentLevel();
+        CreateNextLevel();
+    }
+
+    private void GetLevelInformations()
+    {
+        currentLevelIndex = GameManager.Instance.CurrentLevelIndex;
+        nextLevelIndex = currentLevelIndex + 1;
+
+        currentLevelData = levels[currentLevelIndex];
+        nextLevelData = levels[nextLevelIndex];
+
+        initialPlayerNumber = currentLevelData.initialPlayerNumber;
+    }
+
+    private void CreateCurrentLevel()
+    {
+        Texture2D levelTexture = currentLevelData.levelTexture;
 
         float width = levelTexture.width;
         float height = levelTexture.height;
@@ -51,9 +87,8 @@ public class LevelManager : MonoBehaviour
         // Offset for centering the level on the x axis
         Vector3 offset = new Vector3(width / 2f, 0f, height / 2f) - new Vector3(0.5f, 0f, 0.5f);
 
-        // Level Base
-        GameObject levelBase = Instantiate(levelBasePrefab, levelParent.transform);
-        //uuulevelBase.transform.localScale = new Vector3(width, levelBase.transform.localScale.y, height);
+        currentLevel = Instantiate(levelBasePrefab, levelsParent.transform);
+        currentLevel.name = "CurrentLevel";
 
         for (int x = 0; x < width; x++)
         {
@@ -70,18 +105,81 @@ public class LevelManager : MonoBehaviour
 
                 if (prefab.CompareTag("Player"))
                 {
-                    prefab.transform.parent = null;
+                    prefab.transform.parent = currentLevel.transform;
                 }
                 else if (prefab.CompareTag("WallTile"))
                 {
-                    prefab.transform.parent = levelParent.transform.Find("Walls");
+                    prefab.transform.parent = currentLevel.transform.Find("Walls");
                 }
                 else if (prefab.CompareTag("NumberCube"))
                 {
-                    prefab.transform.parent = levelParent.transform.Find("NumberCubes");
+                    prefab.transform.parent = currentLevel.transform.Find("NumberCubes");
                 }
             }
         }
+    }
+
+    private void CreateNextLevel()
+    {
+        Texture2D levelTexture = nextLevelData.levelTexture;
+
+        float width = levelTexture.width;
+        float height = levelTexture.height;
+
+        // Offset for centering the level on the x axis
+        Vector3 offset = new Vector3(width / 2f, 0f, height / 2f) - new Vector3(0.5f, 0f, 0.5f);
+
+        nextLevel = Instantiate(levelBasePrefab, nextLevelInstantiatePoint.position, Quaternion.identity, levelsParent.transform);
+        nextLevel.name = "NextLevel";
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color pixelColor = levelTexture.GetPixel(x, y);
+
+                if (CompareColors(pixelColor, Color.black))
+                {
+                    continue;
+                }
+
+                GameObject prefab = CreatePrefab(pixelColor, nextLevelInstantiatePoint.position + new Vector3(x, 0f, y), offset);
+
+                if (prefab.CompareTag("Player"))
+                {
+                    prefab.transform.parent = nextLevel.transform;
+                }
+                else if (prefab.CompareTag("WallTile"))
+                {
+                    prefab.transform.parent = nextLevel.transform.Find("Walls");
+                }
+                else if (prefab.CompareTag("NumberCube"))
+                {
+                    prefab.transform.parent = nextLevel.transform.Find("NumberCubes");
+                }
+            }
+        }
+    }
+
+    private void MoveLevels()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(currentLevel.transform.DOMove(levelDestroyPoint.position, moveDuration)
+                    .SetEase(Ease.Linear))
+                .Join(nextLevel.transform.DOMove(Vector3.zero, moveDuration)
+                    .SetEase(Ease.Linear))
+                .OnComplete(() =>
+                {
+                    oldLevel = currentLevel;
+                    currentLevel = nextLevel;
+
+                    GetLevelInformations();
+
+                    CreateNextLevel();
+
+                    Destroy(oldLevel);
+                });
     }
 
     private GameObject CreatePrefab(Color color, Vector3 position, Vector3 offset)
