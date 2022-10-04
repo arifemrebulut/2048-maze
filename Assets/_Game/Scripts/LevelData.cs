2330820c66bef755c8177ac9a4b88e6145ea5101
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-10-04 17:05:12 +0300

    Camera position and rotation added to the level data scriptable

--- Assets/_Game/Scripts/LevelData.cs
+++ Assets/_Game/Scripts/LevelData.cs
@@ -5,5 +5,9 @@ public class LevelData : ScriptableObject
 {
     public int initialPlayerNumber;
 
+    [Header("Camera Settings for Level")]
+    public Vector3 cameraPosition;
+    public Vector3 cameraRotation;
+
     public Texture2D levelTexture;
 }
